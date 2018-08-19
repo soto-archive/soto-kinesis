e0e78345db8284ecf0a151623540f596e788d958
Jonathan McAllister <jonathan@blast.com>
2018-08-18 19:31:23 -0700

    bump core sdk to 2.0.0-rc.1

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
       .library(name: "SwiftAWSKinesis", targets: ["SwiftAWSKinesis"]),
   ],
   dependencies: [
-      .package(url: "https://github.com/noppoMan/aws-sdk-swift-core.git", .upToNextMajor(from: "1.0.0"))
+      .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMajor(from: "2.0.0-rc.1"))
   ],
   targets: [
       .target(name: "SwiftAWSKinesis", dependencies: ["AWSSDKSwiftCore"]),
