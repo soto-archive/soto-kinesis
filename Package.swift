0edb96c6927021df277d1695054d97822446842d
Adam Fowler <adamfowler71@gmail.com>
2019-11-27 15:48:15 +0000

    Version 4.0.0

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
       .library(name: "Kinesis", targets: ["Kinesis"]),
   ],
   dependencies: [
-      .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "3.4.0"))
+      .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.0.0"))
   ],
   targets: [
       .target(name: "Kinesis", dependencies: ["AWSSDKSwiftCore"]),
