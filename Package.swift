525ac98772326417ac32ae69a1735f16502b75aa
Adam Fowler <adamfowler71@gmail.com>
2020-06-02 16:44:47 +0100

    Version 4.7.0

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
 // swift-tools-version:5.0
-
 import PackageDescription
 
 let package = Package(
-  name: "Kinesis",
-  products: [
-      .library(name: "Kinesis", targets: ["Kinesis"]),
-  ],
-  dependencies: [
-      .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.2.0"))
-  ],
-  targets: [
-      .target(name: "Kinesis", dependencies: ["AWSSDKSwiftCore"]),
-  ]
+    name: "Kinesis",
+    platforms: [.iOS(.v12), .tvOS(.v12), .watchOS(.v5)],
+    products: [
+        .library(name: "Kinesis", targets: ["Kinesis"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.6.0"))
+    ],
+    targets: [
+        .target(name: "Kinesis", dependencies: ["AWSSDKSwiftCore"], path: "./Sources/Kinesis"),
+    ]
 )
