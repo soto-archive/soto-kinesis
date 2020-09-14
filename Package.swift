482da62f5e9a73d65cc04c2701b4bfba87449b1f
Adam Fowler <adamfowler71@gmail.com>
2020-09-14 10:53:12 +0100

    Sync with Soto v4.9.0

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "Kinesis", targets: ["Kinesis"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.6.0"))
+        .package(url: "https://github.com/soto-project/soto-core.git", .upToNextMinor(from: "4.7.0"))
     ],
     targets: [
         .target(name: "Kinesis", dependencies: ["AWSSDKSwiftCore"], path: "./Sources/Kinesis"),
